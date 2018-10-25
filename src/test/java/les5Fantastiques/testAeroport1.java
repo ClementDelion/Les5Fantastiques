98f3f0f7cbe48fda0bf68a5ef071d891cf04bba6
Matthieu Gavelle <Matthieu.Gavelle@uqar.ca>
2018-10-25 16:53:34 +0200

    client+passager

---
+++ src/test/java/les5Fantastiques/testAeroport1.java
@@ -0,0 +1,41 @@
+package les5Fantastiques;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testAeroport1 {
+    private static EntityManagerFactory emf = null;
+    private EntityManager em = null;
+    @BeforeClass
+    public static void initEntityManagerFactory() {
+        emf = Persistence.createEntityManagerFactory("les5Fantastiques");
+    }
+    @AfterClass
+    public static void closeEntityManagerFactory() {
+        emf.close();
+    }
+    @Before
+    public void initEntityManager() {
+        em = emf.createEntityManager();
+    }
+    @After
+    public void closeEntityManager() {
+        em.close();
+    }
+    @Test
+    public void test() {
+        assertNotNull(emf);
+        assertNotNull(em);
+    }
+}
+
+
